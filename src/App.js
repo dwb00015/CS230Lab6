bee32361b6657937b4b182ebdc4a9c288e8fa249
dwb00015 <dwb00015@mix.wvu.edu>
2023-10-25 09:42:22 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <h1>CS 230L</h1>
+          <h2>Section - 03</h2>
+          <p>WVU ID: 800376798</p>
+          <p>Hi I am Devin Booth</p>
+      </div>
+  );
+}
+
+export default App;
